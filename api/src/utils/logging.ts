7bbb0ed5a1f01ed59afe015908d7ce29d7fd48c8
alimprasetyo77 <palim695@gmail.com>
2025-07-06 19:44:54 +0700

    Remove error logging file and update logging configuration to only use console transport

--- api/src/utils/logging.ts
+++ api/src/utils/logging.ts
@@ -9,11 +9,7 @@ const logger = createLogger({
       return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
     })
   ),
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
-  ],
+  transports: [new transports.Console()],
 });
 
 export default logger;
