48ea49ba5da7d88314429a4fe95362322c1f2f1c
alimprasetyo77 <palim695@gmail.com>
2025-11-23 17:35:56 +0700

    refactor: Standardize code formatting and update rate limit for cron endpoint

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from "express";
-import { PrismaClient } from "./generated/prisma";
-import { apiRouter } from "./routes/api";
-import { errorMiddleware } from "./middleware/error-middleware";
-import { publicRouter } from "./routes/public-api";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import { v2 as cloudinary } from "cloudinary";
-import { requestLogger } from "./middleware/requestLogger";
-import rateLimit from "express-rate-limit";
+import express, { Request, Response } from 'express';
+import { PrismaClient } from './generated/prisma';
+import { apiRouter } from './routes/api';
+import { errorMiddleware } from './middleware/error-middleware';
+import { publicRouter } from './routes/public-api';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+import { requestLogger } from './middleware/requestLogger';
+import rateLimit from 'express-rate-limit';
 dotenv.config();
 
 const app = express();
@@ -16,14 +16,14 @@ export const prisma = new PrismaClient();
 
 // Configuration
 cloudinary.config({
-  cloud_name: "dcwbezcru",
+  cloud_name: 'dcwbezcru',
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 30,
   standardHeaders: true,
   legacyHeaders: false,
   handler: (_req, res, _next, options) => {
@@ -36,7 +36,7 @@ export const apiLimiter = rateLimit({
 });
 
 const allowedOrigins =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development'
     ? [process.env.URL_DEV, process.env.URL_PROD_DEV]
     : [process.env.URL_PROD];
 
@@ -46,7 +46,7 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error('Not allowed by CORS'));
       }
     },
     credentials: true,
@@ -58,16 +58,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
 });
 app.use(publicRouter);
 app.use(apiRouter);
 
 app.use(errorMiddleware);
 
-if (process.env.NODE_ENV === "development") {
-  app.listen(3000, () => console.log("Listening on port 3000"));
+if (process.env.NODE_ENV === 'development') {
+  app.listen(3000, () => console.log('Listening on port 3000'));
 }
 export default function handler(req: Request, res: Response) {
   app(req, res);
--- api/src/routes/public-api.ts
+++ api/src/routes/public-api.ts
@@ -1,22 +1,30 @@
-import express, { RequestHandler } from "express";
-import { UserController } from "../controllers/user-controller";
-import { PropertyController } from "../controllers/property-controller";
-import { cleanOrphanFile } from "../utils/cron";
+import express, { RequestHandler } from 'express';
+import { UserController } from '../controllers/user-controller';
+import { PropertyController } from '../controllers/property-controller';
+import { cleanOrphanFile } from '../utils/cron';
+import rateLimit from 'express-rate-limit';
 
 export const publicRouter = express.Router();
 
 // Authentication routes
-publicRouter.post("/api/users/login", UserController.login);
-publicRouter.post("/api/users/register", UserController.register);
-publicRouter.post("/api/users/refresh-token", UserController.refreshToken);
-publicRouter.post("/api/users/logout", UserController.logout);
+publicRouter.post('/api/users/login', UserController.login);
+publicRouter.post('/api/users/register', UserController.register);
+publicRouter.post('/api/users/refresh-token', UserController.refreshToken);
+publicRouter.post('/api/users/logout', UserController.logout);
 
 // Property routes
-publicRouter.get("/api/properties", PropertyController.getAll);
-publicRouter.get("/api/properties/location", PropertyController.getLocation);
-publicRouter.post("/api/properties/search", PropertyController.search);
-publicRouter.get("/api/properties/property-of-cities", PropertyController.getCountPropertyEachCities);
-publicRouter.get("/api/properties/:propertyId", PropertyController.getById);
+publicRouter.get('/api/properties', PropertyController.getAll);
+publicRouter.get('/api/properties/location', PropertyController.getLocation);
+publicRouter.post('/api/properties/search', PropertyController.search);
+publicRouter.get('/api/properties/property-of-cities', PropertyController.getCountPropertyEachCities);
+publicRouter.get('/api/properties/:propertyId', PropertyController.getById);
 
 // cron
-publicRouter.get("/api/cron", cleanOrphanFile as RequestHandler);
+publicRouter.get(
+  '/api/cron',
+  rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 1,
+  }),
+  cleanOrphanFile as RequestHandler
+);
